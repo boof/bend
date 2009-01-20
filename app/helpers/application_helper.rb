992726bcaa1deee0e69b3b765acd20a4d9be9a2e
Florian Aßmann <florian.assmann@email.de>
2009-01-20 09:59:56 +0100

    Added eo (even/odd) helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def eo(index, offset = 0, *classes)
+    classes = %w[ even odd ] if classes.empty?
+    modulo = classes.length
+
+    classes.at index % modulo + offset % modulo
+  end
+
 end
