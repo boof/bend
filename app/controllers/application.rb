bae6fc35964ffdcc84e977d98190367262e91bd4
Florian Aßmann <florian.assmann@email.de>
2009-01-20 10:10:17 +0100

    Added example data.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -13,3 +13,19 @@ class ApplicationController < ActionController::Base
   # from your application log (in this case, all fields with names like "password"). 
   filter_parameter_logging :password
 end
+
+Players = Array.new(5) do |i|
+  name = "Player #{ i + 1 }"
+  OpenStruct.new :name => name
+end
+Teams = {
+  'Team 1' => OpenStruct.new(:name => 'Team 1', :players => [ Players[3], Players[0] ]),
+  'Team 2' => OpenStruct.new(:name => 'Team 2', :players => [ Players[1], Players[2], Players[4] ]),
+  'Team 3' => OpenStruct.new(:name => 'Team 3', :players => [ Players[3], Players[4] ]),
+  'Team 4' => OpenStruct.new(:name => 'Team 4', :players => [ Players[0], Players[1], Players[2]])
+}
+Players[0].teams = [ Teams['Team 1'], Teams['Team 4'] ]
+Players[1].teams = [ Teams['Team 2'], Teams['Team 4'] ]
+Players[2].teams = [ Teams['Team 2'], Teams['Team 4'] ]
+Players[3].teams = [ Teams['Team 1'], Teams['Team 3'] ]
+Players[4].teams = [ Teams['Team 2'], Teams['Team 3'] ]
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
 
   def index
+    @matches = current_player.matches.find :all, :order => 'stated_at DESC'
   end
 
 end
