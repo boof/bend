39959aa4dd34a5c4e79916d1e9edb1930e6173b5
Florian Aßmann <florian.assmann@email.de>
2009-01-28 12:08:53 +0100

    Replaced template with method.

--- app/shadows/membership_shadow.rb
+++ app/shadows/membership_shadow.rb
@@ -1,6 +1,15 @@
 class MembershipShadow < Shadows::Base
-  include ApplicationHelper
 
-  p self
+  def player
+    @player ||= @membership.player
+  end
+
+  def gravatar(size = '32x32')
+    image_tag player.gravatar_url(:size => size), :alt => player.name
+  end
+
+  def linked_gravatar(size = '32x32')
+    link_to gravatar(size), player, :title => player.name
+  end
 
 end
