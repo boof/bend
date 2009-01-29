b95cccaac88ea3c40c249a028c4b01ee0a5a1dfc
Florian Aßmann <florian.assmann@email.de>
2009-01-29 10:22:49 +0100

    Assign scopes.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
 
   def index
-    @matches  = current_player.matches.find :all, :order => 'stated_at DESC'
-    @teams    = current_player.teams.find :all, :order => 'name'
+    @matches  = current_player.matches.scoped :order => 'stated_at DESC'
+    @teams    = current_player.teams.scoped :order => 'name'
   end
 
 end
